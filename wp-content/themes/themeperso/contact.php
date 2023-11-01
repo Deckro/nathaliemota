d518fffea30ec50f6209a77a15f263e46af2fbe2
damie <deck_soufly@hotmail.fr>
2023-11-01 15:44:33 +0100

    article

---
+++ wp-content/themes/themeperso/contact.php
@@ -0,0 +1,7 @@
+
+
+<div>
+    <?php 
+    echo do_shortcode('[contact-form-7 id="714e07d" title="formulaire contact"]');
+    ?>
+</div>
--- wp-content/themes/themeperso/home.php
+++ wp-content/themes/themeperso/home.php
@@ -1,10 +1,11 @@
 <?php get_header(); ?>
 
-<h1>photographe event</h1>
 <div>
-    <?php 
-    echo do_shortcode('[contact-form-7 id="ce1814b" title="Sans titre"]');
-    ?>
+    <h1>photographe event</h1>
+</div>
+
+<div class="modale">
+    <?php echo do_shortcode('[contact-form-7 id="714e07d" title="formulaire contact"]');?>
 </div>
 
 
