9c33d8214a84221aad119deed49bdffb7fa69aac
damie <deck_soufly@hotmail.fr>
2023-11-01 16:38:07 +0100

    plugins post

--- wp-content/themes/themeperso/home.php
+++ wp-content/themes/themeperso/home.php
@@ -4,9 +4,7 @@
     <h1>photographe event</h1>
 </div>
 
-<div class="modale">
-    <?php echo do_shortcode('[contact-form-7 id="714e07d" title="formulaire contact"]');?>
-</div>
+
 
 
 <?php get_footer(); ?>
