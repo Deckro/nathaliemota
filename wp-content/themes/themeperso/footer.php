2a01637515d1827341642826332f4553b1c81f1d
damie <deck_soufly@hotmail.fr>
2023-11-06 14:40:40 +0100

    css footer

--- wp-content/themes/themeperso/footer.php
+++ wp-content/themes/themeperso/footer.php
@@ -1,17 +1,13 @@
 <footer>
     <?php require_once 'template-parts/footer/modale-contact.php'; ?>
     <div>
-        <nav>
-        <nav>
+        <nav class="nav_footer">
             <?php $menuf = wp_get_nav_menu_items('footer');
                 for($i = 0; $i< count($menuf); $i++){?>
-                <div>
                    <a href="<?php echo $menuf[$i]-> url;?>"><?php echo $menuf[$i]-> title;?></a>
-                </div>
                 <?php                    
                 }
             ?> 
-            </nav>
         </nav>
     </div>
     
