b942def136ea6a11c8991e45b3d59f81e140ad3d
damie <deck_soufly@hotmail.fr>
2023-11-06 20:29:53 +0100

    home 1ere parti

--- wp-content/themes/themeperso/home.php
+++ wp-content/themes/themeperso/home.php
@@ -7,8 +7,8 @@
     $galerie = new WP_Query($args);
 ?>
 
-<div style="background-image: url(<?php echo get_the_post_thumbnail_url($galerie->posts[5]->ID, 'full');?>)"> 
-    <h1>photographe event</h1>
+<div class="banner" style="background-image: url(<?php echo get_the_post_thumbnail_url($galerie->posts[5]->ID, 'full');?>)"> 
+    <h1 class="titre_banner">photographe event</h1>
 
 </div>
 
@@ -16,16 +16,17 @@
 <select name="formats" id="formats"><option value="">formats</option><option value="#">#</option></select>
 <select name="trier_par" id="trier_par"><option value="trier_par">TRIER PAE</option><option value="#">#</option></select>
 
-<div>
+<div class="home_galerie">
     <?php                    
         while ( $galerie->have_posts() ) {
             $galerie->the_post();
-            echo ' <div class="home_photo">';
-            echo get_the_post_thumbnail( get_the_ID(), 'full' );
+            echo '<div class="home_photos">';
+            echo get_the_post_thumbnail( get_the_ID(), '[1200,800]' );
             echo '</div>';       
         } 
     ?>
 </div>
-<button>charger plus</button>
-
+<div class="charger_btn">
+    <button class="charger">charger plus</button>
+</div>
 <?php get_footer(); ?>
