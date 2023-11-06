3acbb7645b6cba7f9d49e8687049d7e745587142
damie <deck_soufly@hotmail.fr>
2023-11-06 11:01:50 +0100

    fin de l'inversion des information

--- wp-content/themes/themeperso/home.php
+++ wp-content/themes/themeperso/home.php
@@ -1,8 +1,31 @@
 <?php get_header(); ?>
+<?php
+    $args = array(
+        'post_type' => 'photo',
+        'posts_per_page' => -1,
+    );
+    $galerie = new WP_Query($args);
+?>
 
-<div>
+<div style="background-image: url(<?php echo get_the_post_thumbnail_url($galerie->posts[5]->ID, 'full');?>)"> 
     <h1>photographe event</h1>
+
 </div>
 
- 
+<select name="catégorie" id="catégorie"><option value="">Catégorie</option><option value="mariage">mariage</option></select>
+<select name="formats" id="formats"><option value="">formats</option><option value="#">#</option></select>
+<select name="trier_par" id="trier_par"><option value="trier_par">TRIER PAE</option><option value="#">#</option></select>
+
+<div>
+    <?php                    
+        while ( $galerie->have_posts() ) {
+            $galerie->the_post();
+            echo ' <div class="home_photo">';
+            echo get_the_post_thumbnail( get_the_ID(), 'full' );
+            echo '</div>';       
+        } 
+    ?>
+</div>
+<button>charger plus</button>
+
 <?php get_footer(); ?>
--- wp-content/themes/themeperso/single.php
+++ wp-content/themes/themeperso/single.php
@@ -2,6 +2,13 @@
 <?php $fields = get_fields();?>
 <?php $post = get_post();?>
 <?php $img = get_the_post_thumbnail_url();?>
+<?php
+    $args = array(
+        'post_type' => 'photo',
+        'posts_per_page' => -1,
+    );
+    $galerie = new WP_Query($args);
+?>
 <div>
 <h2><?php echo $post->post_title;?></h2>
 <p>Référence :<?php echo $fields['reference'];?></p>
@@ -11,6 +18,27 @@
 <p>Année :<?php echo $fields['année'];?></p>
 </div>
 <img src="<?php echo $img;?>" alt="">
+<div>
+    <p>cette photo vous intéresse?</p>
+    <button><a href="#contact">contact</a></button>
+    <div>
+        <img src="" alt="miniature">
+        <div><img src="" alt=""><img src="" alt=""></div>
+    </div>
+</div>
+<div>
+    <p>VOUS AIMEREZ AUSSI</p>
+    <?php                    
+        while ( $galerie->have_posts() ) {
+            $galerie->the_post();
+            echo ' <div class="single_galerie">';
+            echo get_the_post_thumbnail( get_the_ID(), 'full' );
+            the_title();
+            echo '</div>';       
+        } 
+    ?>
+</div>
+<button id='tout'>Toutes les photos</button>
 
 
 <?php get_footer(); ?>
