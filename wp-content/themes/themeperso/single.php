9f33d6373c4e991dc2d7ec192a4074b049beb197
damie <deck_soufly@hotmail.fr>
2023-11-07 11:28:04 +0100

    single page bottom

--- wp-content/themes/themeperso/single.php
+++ wp-content/themes/themeperso/single.php
@@ -22,24 +22,32 @@
         <img class="single_top_img" src="<?php echo $img;?>" alt="">
     </div>
 </div>
-<div>
-    <p>cette photo vous intéresse?</p>
-    <button><a href="#contact">contact</a></button>
+    
+
+<div class="single_middle">
+    <div class="single_middle_info">
+        <p>cette photo vous intéresse?</p>
+        <button><a href="#contact">contact</a></button>
+    </div>
     <div>
         <img src="" alt="miniature">
         <div><img src="" alt=""><img src="" alt=""></div>
     </div>
 </div>
+
+
 <div>
     <p>VOUS AIMEREZ AUSSI</p>
-    <?php                    
-        while ( $galerie->have_posts() ) {
-            $galerie->the_post();
-            echo ' <div class="single_galerie">';
-            echo get_the_post_thumbnail( get_the_ID(), 'full' );
-            echo '</div>';       
-        } 
-    ?>
+    <div class="single_galerie">
+        <?php                    
+            while ( $galerie->have_posts() ) {
+                $galerie->the_post();
+                echo ' <div class="single_photos">';
+                echo get_the_post_thumbnail( get_the_ID(), 'full' );
+                echo '</div>';       
+            } 
+        ?>
+    </div>
 </div>
 <button id='tout'>Toutes les photos</button>
 
