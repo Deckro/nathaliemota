15704ff10d757f98b3fed658695dc0bc3d6704f7
damie <deck_soufly@hotmail.fr>
2023-11-15 14:28:44 +0100

    menu telephone 1ere partis et filtre

--- wp-content/themes/themeperso/assets/js/filtre.js
+++ wp-content/themes/themeperso/assets/js/filtre.js
@@ -1,5 +1,7 @@
 const tout = document.getElementById('tout')
 const photos = document.getElementsByClassName('photos')
+const filtres = document.getElementsByClassName('filtre')
+const choix = document.getElementById('choix')
 
 tout.addEventListener('click', function(){
     affiche ()
@@ -9,3 +11,13 @@ function affiche (){
     for (let i = 0; i< photos.length; i++) {
     photos[i].classList.remove('cacher')
 }}
+
+function filtre() {
+    for (let i = 0; i< filtres.length; i++){
+        filtres[i].addEventListener('change', function(){
+            console.log('moi')
+            choix.submit()
+        })
+    }
+}
+filtre()
--- wp-content/themes/themeperso/home.php
+++ wp-content/themes/themeperso/home.php
@@ -1,20 +1,60 @@
 <?php get_header(); ?>
 <?php
+    $cat = get_terms('categorie');
+    $for = get_terms('format');
+    $taxo = array();
+    $formats = $_GET['formats'];
+    $categories = $_GET['catégorie'];
+    if($categories != ''){
+        $taxo[] = array ( 
+            'taxonomy' => 'categorie',
+            'field' => 'slug',
+            'terms' => $categories
+        );
+    }
+    if($formats != ''){
+        $taxo[] = array ( 
+            'taxonomy' => 'format',
+            'field' => 'slug',
+            'terms' => $formats
+        );
+    }
     $args = array(
         'post_type' => 'photo',
         'posts_per_page' => -1,
+        'tax_query' => $taxo
     );
     $galerie = new WP_Query($args);
 ?>
 
-<div class="flex center banner" style="background-image: url(<?php echo get_the_post_thumbnail_url($galerie->posts[rand(0,count($galerie->posts)-1)]->ID, 'full');?>)"> 
+<div class="flex center banner" style="background-image: url(<?php echo get_the_post_thumbnail_url($galerie->posts[rand(0,count($galerie->posts)-1)]->ID, '[763,763]');?>)"> 
     <h1 class="flex titre_banner">photographe event</h1>
 
 </div>
 <div class="flex selecteur">
     <div class="flex select_gauche">
-        <select name="catégorie" id="catégorie"><option value="">Catégorie</option><option value="mariage">mariage</option></select>
-        <select name="formats" id="formats"><option value="">formats</option><option value="#">#</option></select>
+        <form id="choix" action="">
+            <select class="filtre" name="catégorie" id="catégorie">
+                <option value="">Catégorie</option>
+                <?php
+                var_dump($categories);
+                for($i=0; $i<count($cat); $i++) {
+                    $selected = $cat[$i]->slug==$categories?"selected":"";
+                    echo '<option '.$selected.' value="'.$cat[$i]->slug.'">'.$cat[$i]->name.'</option>';
+                }
+                ?>
+            </select>
+            <select class="filtre" name="formats" id="formats">
+                <option value="">formats</option>
+                <?php
+                var_dump($formats);
+                for($i=0; $i<count($for); $i++) {
+                    $selecteds = $for[$i]->slug==$formats?"selected":"";
+                    echo '<option '.$selecteds.' value="'.$for[$i]->slug.'">'.$for[$i]->name.'</option>';
+                }
+                ?>
+            </select>
+        </form>
     </div>
     <div class="select_droit">
         <select name="trier_par" id="trier_par"><option value="trier_par">Trier par</option><option value="#">#</option></select>
