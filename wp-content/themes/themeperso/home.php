81b301b38bbd452daeaf474b46cfa5e946eb927b
damie <deck_soufly@hotmail.fr>
2023-11-16 12:08:42 +0100

    version téléphone + filtres categorie et format 2

--- wp-content/themes/themeperso/home.php
+++ wp-content/themes/themeperso/home.php
@@ -3,16 +3,16 @@
     $cat = get_terms('categorie');
     $for = get_terms('format');
     $taxo = array();
-    $formats = $_GET['formats'];
-    $categories = $_GET['catégorie'];
-    if($categories != ''){
+    $formats = isset($_GET['formats'])?$_GET['formats']:null;
+    $categories = isset($_GET['catégorie'])?$_GET['catégorie']:null;
+    if($categories != '' && $categories != null){
         $taxo[] = array ( 
             'taxonomy' => 'categorie',
             'field' => 'slug',
             'terms' => $categories
         );
     }
-    if($formats != ''){
+    if($formats != '' && $formats != null){
         $taxo[] = array ( 
             'taxonomy' => 'format',
             'field' => 'slug',
@@ -69,7 +69,14 @@
         while ( $galerie->have_posts() ) {  
             $galerie->the_post();
             $cacher = $compteur>7?"cacher":"";
-            echo '<div class="flex photos '.$cacher.'"><a href="'.get_post_permalink().'">';
+            echo '<div class="flex photos '.$cacher.'">
+            <div class="hover">
+                <img id="" class="cadre" src="'.get_template_directory_uri().'/assets/img/icon_fullscreen.svg'.'" alt="">
+                <img id="" class="eye" src="'.get_template_directory_uri().'/assets/img/icon_eye.svg'.'" alt="">
+                <p class="titre_img">'.get_the_title().'</p>
+                <p class="categorie_img">'.get_the_terms(get_the_ID(), 'categorie')[0]->name.'</p>
+            </div>
+            <a href="'.get_post_permalink().'">';
             echo get_the_post_thumbnail( get_the_ID(), 'full' );
             echo '</a></div>';  
             $compteur ++;       
